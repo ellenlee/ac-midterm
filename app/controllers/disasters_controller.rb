db60aaada48154deadde1ab8a2948d5dbefeeb44
盧柏嘉 <lupochia@LuPaul.local>
2017-03-24 15:14:04 +0800

    gem devise & bootstrap and create model disaster

---
+++ app/controllers/disasters_controller.rb
@@ -0,0 +1,5 @@
+class DisastersController < ApplicationController
+  def index
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root 'disasters#index'
+  resources :disasters
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
